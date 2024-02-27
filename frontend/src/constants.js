bf28e502e1ad3cda728fe0bcf6388b7174d3fed5
ecallhealth <ecallhealth@gmail.com>
2024-02-27 18:27:03 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'https://health-care-wvs3.onrender.com/api/'; // If using proxy
+export const BASE_URL = 'https://health-care-wvs3.onrender.com/'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
