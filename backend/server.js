dfc1cc17179b6c9c92521aac0d9c103d5093e5c4
ecallhealth <ecallhealth@gmail.com>
2024-02-27 20:59:58 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,8 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-app.use(
-  cors({ origin: 'https://health-care-wvs3.onrender.com/', credentials: true })
-);
-
+app.use(cors({ origin: 'https://health-care-wvs3.onrender.com/' }));
+app.options('*', cors());
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
