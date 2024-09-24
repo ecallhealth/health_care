e3ff7a895bdb0e8a709b779ca0fefee330ee947a
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-24 17:47:09 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,6 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://ecallapi.embagga.com';
-// export const BASE_URL = 'http://localhost:5000';
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const BASE_URL = '';  // Empty because front-end and back-end are on the same domain
+
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
