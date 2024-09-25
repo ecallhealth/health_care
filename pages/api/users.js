345e847cd4828389866e96359a88e0a3fdf4459f
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-25 16:33:12 +0300

    removed

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,1 @@
-import users from '../../backend/data/users';  // Importing user data
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(users);  // Return users as JSON
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
