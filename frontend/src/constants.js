0408b5dbc0fcc8bebf9dc326f7646272da79917f
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-25 16:59:23 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,4 @@
-export const BASE_URL = '';  // Empty because front-end and back-end are on the same domain
-
-export const PRODUCTS_URL = `${BASE_URL}/api/products`;
-export const USERS_URL = `${BASE_URL}/api/users`;
-export const ORDERS_URL = `${BASE_URL}/api/orders`;
-export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'
+    : '';  // Keep empty for production on Vercel
