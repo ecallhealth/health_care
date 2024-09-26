418bc9fea2b069bf7adf861ff9e836147581fadb
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-26 16:58:51 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,10 @@
-// Update BASE_URL to point to your Render back-end URL
-export const BASE_URL = 'https://health-care-backend-yxrm.onrender.com';  // Render back-end URL
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000'   // For local development
+    : 'https://health-care-backend-yxrm.onrender.com';  // Render URL for production
 
 // API endpoints
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
 export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
-
