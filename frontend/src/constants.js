11ad2b9d9e75d89c5946bb7cacec555217f05cda
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-26 12:47:04 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,9 @@
-export const BASE_URL = '';  // Empty because front-end and back-end are on the same domain
+// Update BASE_URL to point to your Render back-end URL
+export const BASE_URL = 'https://health-care-wvs3.onrender.com';  // Render back-end URL
 
+// API endpoints
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
 export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+
