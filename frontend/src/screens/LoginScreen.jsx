f8336dd780da336fd7f664368727e0cf99ff7646
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-27 07:27:54 -0700

    Update LoginScreen.jsx

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -4,7 +4,6 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
@@ -15,11 +14,8 @@ const LoginScreen = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [login, { isLoading }] = useLoginMutation();
-
   const { userInfo } = useSelector((state) => state.auth);
-
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
   const redirect = sp.get('redirect') || '/';
@@ -34,59 +30,54 @@ const LoginScreen = () => {
     e.preventDefault();
     try {
       const res = await login({ email, password }).unwrap();
-      dispatch(setCredentials({ ...res }));
-      navigate(redirect);
+      dispatch(setCredentials({ ...res })); // Dispatch user data and token to Redux store
+      localStorage.setItem('token', res.token); // Store the token in local storage
+      navigate(redirect); // Redirect after successful login
     } catch (err) {
       toast.error(err?.data?.message || err.error);
     }
   };
 
   return (
-   <FormContainer>
-  <h1>Sign In</h1>
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-  <Form onSubmit={submitHandler}>
-    <Form.Group className='my-2' controlId='email'>
-      <Form.Label>Email Address</Form.Label>
-      <Form.Control
-        type='email'
-        placeholder='Enter email'
-        value={email}
-        name="email"  // Add name attribute
-        autocomplete="email"  // Add autocomplete attribute
-        onChange={(e) => setEmail(e.target.value)}
-      ></Form.Control>
-    </Form.Group>
+        <Form.Group className='my-2' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-    <Form.Group className='my-2' controlId='password'>
-      <Form.Label>Password</Form.Label>
-      <Form.Control
-        type='password'
-        placeholder='Enter password'
-        value={password}
-        name="password"  // Add name attribute
-        autocomplete="current-password"  // Add autocomplete attribute
-        onChange={(e) => setPassword(e.target.value)}
-      ></Form.Control>
-    </Form.Group>
+        <Button disabled={isLoading} type='submit' variant='primary'>
+          Sign In
+        </Button>
 
-    <Button disabled={isLoading} type='submit' variant='primary'>
-      Sign In
-    </Button>
+        {isLoading && <Loader />}
+      </Form>
 
-    {isLoading && <Loader />}
-  </Form>
-
-  <Row className='py-3'>
-    <Col>
-      New Customer?{' '}
-      <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-        Register
-      </Link>
-    </Col>
-  </Row>
-</FormContainer>
-
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
   );
 };
 
