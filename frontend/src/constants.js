bd73ad79a48b360a0e988da13f4b1fcb48a09d90
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-29 18:52:21 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,10 +1,10 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'   // For local development
-    : 'https://health-care-backend-yxrm.onrender.com';  // Render URL for production
+// src/constants.js
+
+// Base URL for the production environment from environment variable
+export const BASE_URL = process.env.BASE_URL; // Set this to the production URL in your .env file
 
 // API endpoints
-export const PRODUCTS_URL = ${BASE_URL}/api/products;
-export const USERS_URL = ${BASE_URL}/api/users;
-export const ORDERS_URL = ${BASE_URL}/api/orders;
-export const PAYPAL_URL = ${BASE_URL}/api/config/paypal;
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
