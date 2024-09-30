ca3c480a9f2f3a03fec012ab41cda3a105952673
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-09-30 14:55:53 +0300

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -6,7 +6,7 @@ export const BASE_URL =
 // Adjust the API endpoints based on what actually exists on your back-end
 // Uncomment or add back the routes if you implement them later
 
-export const PRODUCTS_URL = `${BASE_URL}/routes/productRoutes`;  // Uncomment if you have this route
+ export const PRODUCTS_URL = `${BASE_URL}/api/products`;  // Uncomment if you have this route
  export const USERS_URL = `${BASE_URL}/api/users`;  // Uncomment if you have this route
-export const ORDERS_URL = `${BASE_URL}/routes/orderRoutes`;  // Uncomment if you have this route
- export const PAYPAL_URL = `${BASE_URL}/utils/paypal`;  // Uncomment if you have this route
+ export const ORDERS_URL = `${BASE_URL}/api/orders`;  // Uncomment if you have this route
+ export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;  // Uncomment if you have this route
