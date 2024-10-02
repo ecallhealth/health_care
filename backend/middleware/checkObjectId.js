a6872667d50030dba63b48333a6ea2a1c84f3612
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-10-02 19:31:16 +0300

    Update checkObjectId.js

--- backend/middleware/checkObjectId.js
+++ backend/middleware/checkObjectId.js
@@ -7,13 +7,10 @@ import { isValidObjectId } from 'mongoose';
  * @param {import('express').Request} req - The Express request object.
  * @param {import('express').Response} res - The Express response object.
  * @param {import('express').NextFunction} next - The Express next middleware function.
- * @throws {Error} Throws an error if the ObjectId is invalid.
  */
-
 function checkObjectId(req, res, next) {
   if (!isValidObjectId(req.params.id)) {
-    res.status(404);
-    throw new Error(`Invalid ObjectId of:  ${req.params.id}`);
+    return res.status(400).json({ message: `Invalid ObjectId of: ${req.params.id}` });
   }
   next();
 }
