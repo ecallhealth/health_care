fe2fe04471aa066eb6734491876179ab74fe3023
ecallhealth <161455854+ecallhealth@users.noreply.github.com>
2024-10-04 19:23:44 +0300

    Update HomeScreen.jsx

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,6 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
-import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -12,7 +11,7 @@ import SlidingCarousel from '../components/slidingCarousel';
 import AdvertisingCarousel from '../components/AdvertisingBanner';
 
 const HomeScreen = () => {
-  const { pageNumber, keyword } = useParams();
+  const { keyword, pageNumber } = useParams();
 
   const { data, isLoading, error } = useGetProductsQuery({
     keyword,
@@ -28,6 +27,7 @@ const HomeScreen = () => {
           Go Back
         </Link>
       )}
+
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -37,14 +37,20 @@ const HomeScreen = () => {
       ) : (
         <>
           <Meta />
-          <h1>Latest Products</h1>
-          <Row>
-            {data.products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
+          <h1>{keyword ? `Search Results for '${keyword}'` : 'Latest Products'}</h1>
+
+          {data.products.length === 0 ? (
+            <Message>No products found for the search term "{keyword}"</Message>
+          ) : (
+            <Row>
+              {data.products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )}
+
           <Paginate
             pages={data.pages}
             page={data.page}
@@ -52,37 +58,9 @@ const HomeScreen = () => {
           />
         </>
       )}
-      {!keyword ? (
-        <AdvertisingCarousel />
-      ) : (
-        <Link to='/' className='btn btn-light mb-4'>
-          Go Back
-        </Link>
-      )}
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>
-          {error?.data?.message || error.error}
-        </Message>
-      ) : (
-        <>
-          <Meta />
-          <h1>Latest Products</h1>
-          <Row>
-            {data.products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-          <Paginate
-            pages={data.pages}
-            page={data.page}
-            keyword={keyword ? keyword : ''}
-          />
-        </>
-      )}
+
+      {!keyword ? <AdvertisingCarousel /> : null}
+
       <SlidingCarousel />
     </>
   );
